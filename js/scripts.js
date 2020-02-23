3df07b90e3e16ff43fc6e332066e65e4add778d5
Jason M <jbird206@gmail.com>
2020-02-22 23:37:57 -0800

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,9 @@ function PizzaParlor() {
   this.currentId = 0
 }
 
-PizzaParlor.prototype.addAccount = function (account){
-account.id = this.assignId();
-this.account.push(account);
+PizzaParlor.prototype.addAccount = function(account) {
+  account.id = this.assignId();
+  this.account.push(account);
 }
 
 PizzaParlor.prototype.assignId = function() {
@@ -13,14 +13,24 @@ PizzaParlor.prototype.assignId = function() {
   return this.currentId;
 }
 
-///////////////////////////////////////Biz////////////////////
-var Account = function (firstName, lastName, myAddress) {
+
+
+
+var Account = function(firstName, lastName, myAddress, price) {
   this.firstName = firstName,
-  this.lastName = lastName,
-  this.address = myAddress;
-  return this.firstName + this.lastName + this.address;
+    this.lastName = lastName,
+    this.address = myAddress,
+    this.price = price;
+  return this.firstName + this.lastName + this.address + this.price;
 };
 
+var Order = function(firstName, lastName, myAddress, myPrice) {
+  this.firstName = firstName,
+    this.lastName = lastName,
+    this.address = myAddress,
+    this.myprice = myPrice;
+  return this.firstName + this.lastName + this.address + this.myPrice;
+};
 
 
 
@@ -28,39 +38,34 @@ var Account = function (firstName, lastName, myAddress) {
 ///////////////////////////////////front end///////////////////////
 var PizzaParlor = new PizzaParlor();
 
+
 $(document).ready(function() {
-//create account//
+  //create account//
   $("form#create-account").submit(function(event) {
     event.preventDefault();
     var inputfirstname = $("input#firstname").val();
     var inputlastname = $("input#lastname").val();
     var inputaddress = $("input#address").val();
-
-    var createAccount = new Account(inputfirstname, inputlastname, inputaddress);
+    var inputprice = $('input[class="calc"]:checked').val();
+    var createAccount = new Account(inputfirstname, inputlastname, inputaddress + inputprice);
 
     PizzaParlor.addAccount(createAccount);
-    $("#outputName").html(inputfirstname + " " + inputlastname + " " + inputaddress);
+    $("#outputName").html(inputfirstname + " " + inputlastname + " " + inputaddress + inputprice);
     $("output").html(createAccount.address);
-  
-});
-
-//add toppings////////////
-
-// $("form#toppings").submit(function(event) {
-//   event.preventDefault();
-
-// })
-
-function calcscore(){
-  var score = 0;
-  $(".calc:checked").each(function(){
-      score+=parseInt($(this).val(),10);
   });
-  $("input[name=sum]").val(score)
-}
-$().ready(function(){
-  $(".calc").change(function(){
-      calcscore()
+
+
+  function calcprice() {
+    var price = 0;
+    $(".calc:checked").each(function() {
+      price += parseInt($(this).val(), 10);
+    });
+
+    $("input#output").val(price)
+  }
+  $().ready(function() {
+    $(".calc").change(function() {
+      calcprice()
+    });
   });
-});
 })
